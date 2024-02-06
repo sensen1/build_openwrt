d64040babf2e34d41c41406537e07abf02e79f10
vaio sensen <fz_bhu@163.com>
2024-02-07 00:13:08 +0800

    提交n1修改。

--- scripts/immortalwrt-18.06-k5.4/02_prepare_package.sh
+++ scripts/immortalwrt-18.06-k5.4/02_prepare_package.sh
@@ -20,17 +20,6 @@ sed -i 's,\"nas\",\"services\",g' package/feeds/luci/luci-app-samba4/luasrc/cont
 sed -i 's,\"system\",\"services\",g' package/feeds/luci/luci-app-cpufreq/luasrc/controller/cpufreq.lua
 # hd-idle
 sed -i 's,\"nas\",\"services\",g' package/feeds/luci/luci-app-hd-idle/luasrc/controller/hd_idle.lua
-# vsftpd
-sed -i -e 's,\"nas\",\"services\",g' -e 's,NAS,Services,g' package/feeds/luci/luci-app-vsftpd/luasrc/controller/vsftpd.lua
-# filebrowser
-sed -i -e 's,\"nas\",\"services\",g' -e 's,NAS,Services,g' package/feeds/luci/luci-app-filebrowser/luasrc/controller/filebrowser.lua
-sed -i 's,nas,services,g' package/feeds/luci/luci-app-filebrowser/luasrc/view/filebrowser/filebrowser_status.htm
-# rclone
-sed -i -e 's,\"nas\",\"services\",g' -e 's,NAS,Services,g' package/feeds/luci/luci-app-rclone/luasrc/controller/rclone.lua
-# dockerman
-pushd package/feeds/luci/luci-app-dockerman
-docker_2_services
-popd
 # nlbw
 sed -i -e 's|admin\",|& \"network\",|g' -e 's,admin/,&network/,g' package/feeds/luci/luci-app-nlbwmon/luasrc/controller/nlbw.lua
 sed -i 's,admin/,&network/,g' package/feeds/luci/luci-app-nlbwmon/luasrc/model/cbi/nlbw/config.lua
@@ -46,9 +35,5 @@ sed -i '/start()/a \    echo "Service autotimesetrun started!" >/dev/null' packa
 rm -rf ./package/sirpdboy/luci-app-autotimeset/po/zh_Hans
 # v2raya
 git clone -b 18.06 --depth 1 https://github.com/zxlhhyccc/luci-app-v2raya.git package/new/luci-app-v2raya
-# verysync
-pushd package/feeds/luci/luci-app-verysync
-move_2_services nas
-popd
 
 exit 0
