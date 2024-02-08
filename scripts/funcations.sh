c33f06793be64d7f7f6f9e38d6f7c490bd190db6
vaio sensen <fz_bhu@163.com>
2024-02-08 22:28:01 +0800

    23.05 for N1 try

--- scripts/funcations.sh
+++ scripts/funcations.sh
@@ -13,6 +13,9 @@ lienol_pkg_repo="https://github.com/Lienol/openwrt-package"
 openwrt_add_repo="https://github.com/QiuSimons/OpenWrt-Add.git"
 passwall_pkg_repo="https://github.com/xiaorouji/openwrt-passwall-packages"
 passwall_luci_repo="https://github.com/xiaorouji/openwrt-passwall"
+dockerman_repo="https://github.com/lisaac/luci-app-dockerman"
+diskman_repo="https://github.com/lisaac/luci-app-diskman"
+docker_lib_repo="https://github.com/lisaac/luci-lib-docker"
 mosdns_repo="https://github.com/sbwml/luci-app-mosdns"
 mosdns_pkg="https://github.com/sbwml/v2ray-geodata"
 sirpdboy_repo="https://github.com/sirpdboy/sirpdboy-package"
@@ -25,4 +28,27 @@ clone_repo() {
     git clone -b $branch_name --depth 1 $repo_url $target_dir
 }
 
+move_2_services() {
+    local resource_file="$({ find | grep "\.lua\|\.htm\|\.json"; } 2>"/dev/null")"
+    for a in ${resource_file}
+    do
+        [ -n "$(grep "\"$1\"" "$a")" ] && sed -i "s,\"$1\",\"services\",g" "$a"
+        [ -n "$(grep "\"${1^^}\"" "$a")" ] && sed -i "s,\"${1^^}\",\"Services\",g" "$a"
+        [ -n "$(grep "\"${1^}\"" "$a")" ] && sed -i "s,\"${1^}\",\"Services\",g" "$a"
+        [ -n "$(grep "\[\[$1\]\]" "$a")" ] && sed -i "s,\[\[$1\]\],\[\[services\]\],g" "$a"
+        [ -n "$(grep "admin/$1" "$a")" ] && sed -i "s,admin/$1,admin/services,g" "$a"
+    done
+}
 
+docker_2_services() {
+    local resource_file="$({ find | grep "\.lua\|\.htm"; } 2>"/dev/null")"
+    local dockerman_lua="$({ find | grep "dockerman\.lua"; } 2>"/dev/null")"
+    for a in ${resource_file}
+    do
+        [ -n "$(grep 'admin\",' "$a")" ] && sed -i "s|admin\",|& \"services\",|g" "$a"
+        [ -n "$(grep 'config\")' "$a")" ] && sed -i "s,config\"),overview\"),g" "$a"
+        [ -n "$(grep 'admin/' "$a")" ] && sed -i "s,admin/,&services/,g" "$a"
+        [ -n "$(grep 'admin\\/' "$a")" ] && sed -i "s,admin\\/,&services\\/,g" "$a"
+    done
+    sed -i 's,Docker,&Man,' ${dockerman_lua}
+}
